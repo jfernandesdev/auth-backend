a567b7f904820c0824a2470fba74bc79012832da
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-10 12:19:37 -0300

    FEAT: login form styling

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,9 +7,20 @@ export default function Home() {
         <title>Auth - Hello World</title>
       </Head>
 
-      <section className='center'>
-        <h1>Hello World!</h1>
-      </section>
+      <div className='container'>
+        <form>
+          <div className='form__group field'>
+            <input type='email' className='form__field' name='email' id='email' placeholder='E-mail'  required/>
+            <label htmlFor="email" className='form__label'>E-mail</label>
+          </div>
+
+          <div className='form__group field'>
+            <input type='password' name='password' id='password' className='form__field' placeholder='Senha' required />
+            <label htmlFor="password" className='form__label'>Senha</label>
+          </div>
+          <button type='submit'>Entrar</button>
+        </form>
+      </div>
     </>
   )
 }
