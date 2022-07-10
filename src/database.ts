c808d9d65efd976486286827eadfcbf0615228e4
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-10 10:07:52 -0300

    INIT: first commit of API RESTful

---
+++ src/database.ts
@@ -0,0 +1,41 @@
+import { RefreshTokensStore, UsersStore } from "./types"
+import { v4 as uuid } from 'uuid'
+
+export const users: UsersStore = new Map()
+
+export const tokens: RefreshTokensStore = new Map()
+
+export function seedUserStore() {
+  users.set('jfernandes.dev@gmail.com', {
+    password: '1234567890',
+    permissions: ['users.list', 'users.create', 'metrics.list'],
+    roles: ['administrator']
+  })
+
+  users.set('fulano@teste.com', {
+    password: '12345',
+    permissions: ['users.list', 'metrics.list'],
+    roles: ['editor']
+  })
+}
+
+export function createRefreshToken(email: string) {
+  const currentUserTokens = tokens.get(email) ?? []
+  const refreshToken = uuid()
+
+  tokens.set(email, [...currentUserTokens, refreshToken])
+
+  return refreshToken;
+}
+
+export function checkRefreshTokenIsValid(email: string, refreshToken: string) {
+  const storedRefreshTokens = tokens.get(email) ?? []
+
+  return storedRefreshTokens.some(token => token === refreshToken)
+}
+
+export function invalidateRefreshToken(email: string, refreshToken: string) {
+  const storedRefreshTokens = tokens.get(email) ?? []
+
+  tokens.set(email, storedRefreshTokens.filter(token => token !== refreshToken));
+}
