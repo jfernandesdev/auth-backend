f094b628a2d49de0bc1229d51c3d4032951cbb9e
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-10 10:52:02 -0300

    INIT: created project with next using template typescript

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Auth - Hello World</title>
+      </Head>
+
+      <section className='center'>
+        <h1>Hello World!</h1>
+      </section>
+    </>
+  )
+}
