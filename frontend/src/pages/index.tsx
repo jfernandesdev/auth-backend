15fe7e6dd2843c24a1097ffb8d299dba4c736a49
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-12 18:43:04 -0300

    FEAT: validating visitor

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useState } from 'react'
 import { parseCookies } from 'nookies'
 
 import { useAuth } from '../hooks/useAuth'
+import { withSSRGuest } from '../utils/withSSRGuest'
 
 export default function Home() {
   const [email, setEmail] = useState('')
@@ -68,19 +69,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
-  
-  if(cookies['appnextauth@token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
-    props: {}
+    props: { }
   }
-}
+});
