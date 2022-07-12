5765752a56cb92b756ff515227bd5ad73aa351c8
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-12 19:26:51 -0300

    FEAT: validating authentication (server)

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from 'react'
 import { useAuth } from '../hooks/useAuth'
-import { api } from '../services/api'
 import { signOut } from '../contexts/AuthContext'
+
+import { api } from '../services/apiClient'
+import { setupAPIClient } from '../services/api'
+
+import { withSSRAuth } from '../utils/withSSRAuth'
 
 export default function Dashboard() {
   const { user } = useAuth()
@@ -27,3 +31,14 @@ export default function Dashboard() {
     </div>
   )
 }
+
+export const getServerSideProps = withSSRAuth( async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+  const response = await apiClient.get('/me')
+
+  console.log(response.data)
+
+  return {
+    props: {}
+  }
+})
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -6,90 +6,99 @@ interface AxiosErrorResponse {
   code?: string
 }
 
-let cookies = parseCookies()
 let isRefreshing = false
 let failedRequestsQueue = []
 
-export const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  headers: {
-    Authorization: `Bearer ${cookies['appnextauth@token']}`,
-  },
-})
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx)
 
-//primeiro parâmetro: o que fazer se a resposta do backend for sucesso?
-//Fazer nada apenas retorna a resposta do backend
-//segundo parâmetro: o que fazer se a resposta do backend der um erro?
-//Pausar todas as requisições (fila), verificar o tipo do erro e atualização o token (refresh token)
-api.interceptors.response.use(
-  response => {
-    return response
-  },
-  (error: AxiosError<AxiosErrorResponse>) => {
-    if (error.response.status === 401) {
-      if (error.response.data?.code === 'token.expired') {
-        //renovar o token
-        cookies = parseCookies()
-        const { 'appnextauth@refreshToken': refreshToken } = cookies
-        const originalConfig = error.config
+  const api = axios.create({
+    baseURL: 'http://localhost:3333',
+    headers: {
+      Authorization: `Bearer ${cookies['appnextauth@token']}`,
+    },
+  })
 
-        if (!isRefreshing) {
-          isRefreshing = true
+  //primeiro parâmetro: o que fazer se a resposta do backend for sucesso?
+  //Fazer nada apenas retorna a resposta do backend
+  //segundo parâmetro: o que fazer se a resposta do backend der um erro?
+  //Pausar todas as requisições (fila), verificar o tipo do erro e atualização o token (refresh token)
+  api.interceptors.response.use(
+    response => {
+      return response
+    },
+    (error: AxiosError<AxiosErrorResponse>) => {
+      if (error.response.status === 401) {
+        if (error.response.data?.code === 'token.expired') {
+          //renovar o token
+          cookies = parseCookies(ctx)
+          const { 'appnextauth@refreshToken': refreshToken } = cookies
+          const originalConfig = error.config
 
-          api
-            .post('/refresh', {
-              refreshToken,
-            })
-            .then(response => {
-              const { token } = response.data
+          if (!isRefreshing) {
+            isRefreshing = true
 
-              setCookie(undefined, 'appnextauth@token', token, {
-                maxAge: 60 * 60 * 24 * 30, // 30 days
-                path: '/',
+            console.log('refresh')
+
+            api
+              .post('/refresh', {
+                refreshToken,
               })
+              .then(response => {
+                const { token } = response.data
 
-              setCookie(
-                undefined,
-                'appnextauth@refreshToken',
-                response.data.refreshToken,
-                {
+                setCookie(ctx, 'appnextauth@token', token, {
                   maxAge: 60 * 60 * 24 * 30, // 30 days
                   path: '/',
+                })
+
+                setCookie(ctx,'appnextauth@refreshToken', response.data.refreshToken, {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/',
+                  }
+                )
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+                failedRequestsQueue.forEach(request => request.onSuccess(token))
+                failedRequestsQueue = []
+              })
+              .catch(err => {
+                failedRequestsQueue.forEach(request => request.onFailure(err))
+                failedRequestsQueue = []
+
+                if (process.browser) {
+                  signOut()
                 }
-              )
+              })
+              .finally(() => {
+                isRefreshing = false
+              })
+          }
 
-              api.defaults.headers['Authorization'] = `Bearer ${token}`
+          return new Promise((resolve, reject) => {
+            failedRequestsQueue.push({
+              onSuccess: (token: string) => {
+                originalConfig.headers['Authorization'] = `Bearer ${token}`
 
-              failedRequestsQueue.forEach(request => request.onSuccess(token))
-              failedRequestsQueue = []
+                resolve(api(originalConfig))
+              },
+              onFailure: (err: AxiosError) => {
+                reject(err)
+              },
             })
-            .catch(err => {
-              failedRequestsQueue.forEach(request => request.onFailure(err))
-              failedRequestsQueue = []
-            })
-            .finally(() => {
-              isRefreshing = false
-            })
-        }
-
-        return new Promise((resolve, reject) => {
-          failedRequestsQueue.push({
-            onSuccess: (token: string) => {
-              originalConfig.headers['Authorization'] = `Bearer ${token}`
-
-              resolve(api(originalConfig))
-            },
-            onFailure: (err: AxiosError) => {
-              reject(err)
-            },
           })
-        })
-      } else {
-        //deslogar o usuário
-        signOut()
+        } else {
+          //deslogar o usuário
+          if (process.browser) {
+            signOut()
+          }
+        }
       }
-    }
 
-    return Promise.reject(error)
-  }
-)
+      return Promise.reject(error)
+    }
+  )
+
+  return api;
+}
